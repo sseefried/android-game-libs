37ef36708721d11c15a28f8cde3eb615e955086a
Sean Seefried <sean.seefried@gmail.com>
2014-11-01 18:05:02 +1100

    Add files reqired to build libcairo

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,3 +1,4 @@
-LOCAL_PATH := $(call my-dir)
+THIS_LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-include $(LOCAL_PATH)/pixman-0.32.6/Android.mk
+include $(THIS_LOCAL_PATH)/pixman-0.32.6/Android.mk
+include $(THIS_LOCAL_PATH)/cairo-1.12.16/Android.mk
